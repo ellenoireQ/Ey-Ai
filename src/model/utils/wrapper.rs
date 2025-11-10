7e32286a69604296e437121031dd169a5836de78
ellenoireQ <ketchupnaiso@gmail.com>
2025-11-10 22:30:33 +0700

    EY-Ai: eyai_wrapper: return to Message struct

--- src/model/utils/wrapper.rs
+++ src/model/utils/wrapper.rs
@@ -1,11 +1,11 @@
 use axum::{Json, extract::State};
 use serde_json::Value;
 
-use crate::model::models::Gemini::{GeminiClient, PromptInput};
+use crate::model::{message::message::Message, models::Gemini::{GeminiClient, PromptInput}};
 
 // A wrapper function for EY-Ai integration
 // it will handle the key management and request generation
-pub async fn eyai_wrapper(State(gemini): State<GeminiClient>, Json(input): Json<PromptInput>) -> Json<Value> {
+pub async fn eyai_wrapper(State(gemini): State<GeminiClient>, Json(input): Json<PromptInput>) -> Json<Message> {
     // Using the question in the state
     let prompt = input.prompt.clone();
     gemini.generate(prompt).await
