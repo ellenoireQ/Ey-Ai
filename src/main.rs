f19a135523d75f00b90f99fdca4f46fada5aa559
ellenoireQ <ketchupnaiso@gmail.com>
2025-11-10 17:13:51 +0700

    EY-Ai: First to try the code changes

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
-use axum::{Json, Router, extract::State, routing::get};
-use serde_json::Value;
-
-use crate::model::models::Gemini::GeminiClient;
+use axum::{ Router, routing::{post}};
+use crate::model::{models::Gemini::GeminiClient, utils::wrapper::eyai_wrapper,};
 mod model;
-
-async fn handler(State(api): State<GeminiClient>) -> Json<Value> {
-    api.generate("halo dunia".to_string()).await
-}
 
 #[tokio::main]
 async fn main() {
     let gemini = GeminiClient::new();
-    gemini.initiate("H".to_string());
-    let key = gemini.generate("Hello, Gemini!".to_string());
-    println!("Gemini key initiated: {:?}", key.await);
+    gemini.initiate("your-env".to_string());
 
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(handler))
+        .route("/generate", post(eyai_wrapper))
         .with_state(gemini.clone());
 
-
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
