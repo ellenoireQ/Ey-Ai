05893833d0e885300922bf4f2c61197d57f77db8
ellenoireQ <ketchupnaiso@gmail.com>
2025-11-10 22:56:52 +0700

    EY-Ai: wrapper: better documentation

--- src/model/utils/wrapper.rs
+++ src/model/utils/wrapper.rs
@@ -3,10 +3,16 @@ use serde_json::Value;
 
 use crate::model::{message::message::Message, models::Gemini::{GeminiClient, PromptInput}};
 
-// A wrapper function for EY-Ai integration
-// it will handle the key management and request generation
+/// A wrapper function for EY-Ai integration.
+///
+/// This function will handle both the **API key management**
+/// and request generation for model calls.
+///
+/// Future Plan
+/// In future versions, this function is planned to become a universal interface
+/// not limited to Gemini, allowing integration with multiple AI models
+/// under a single standardized abstraction layer.
 pub async fn eyai_wrapper(State(gemini): State<GeminiClient>, Json(input): Json<PromptInput>) -> Json<Message> {
-    // Using the question in the state
     let prompt = input.prompt.clone();
     gemini.generate(prompt).await
 }
