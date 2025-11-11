403ecafc1bf39973b8472bcc115c88eef5486ad3
ellenoireQ <ketchupnaiso@gmail.com>
2025-11-11 08:59:09 +0700

    Add doc to message

--- src/model/message/message.rs
+++ src/model/message/message.rs
@@ -1,5 +1,23 @@
 use serde::{Deserialize, Serialize};
 
+/* 
+Message:
+ return example:
+    {
+    "id":"pass",
+    "models":"pass",
+    "question":"Explain how AI works in a few words",
+    "choice":{
+        "role":{
+            "role":"pass",
+            "content":"AI learns patterns from data to make decisions or predictions."
+            }
+        },
+    "timestamp":"pass",
+    "loading":true
+    }
+    NOTE: For now return json will be passed
+*/
 #[derive(Serialize, Deserialize, Default)]
 pub struct Message{
     pub id: String,
